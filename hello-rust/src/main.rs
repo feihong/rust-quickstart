37cc202d01a63e94ed722ced605bb48fc2c10b41
Feihong Hsu <feihong.hsu@gmail.com>
2021-02-21 14:19:32 -0600

    Generate random hanzi

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,7 +1,12 @@
+use rand::Rng;
 use ferris_says::say;
 use std::io::{stdout, BufWriter};
 
 fn main() {
+    let mut rng = rand::thread_rng();
+    let c = rng.gen_range('\u{4e00}'..'\u{a000}');
+    println!("{}", c);
+
     let stdout = stdout();
     let message = String::from("你喜欢吃螃蟹吗？");
     let width = message.chars().count();
