b2965a3bd848e64fece97f3b5c6eeb807857bbe3
Feihong Hsu <feihong.hsu@gmail.com>
2021-02-21 14:56:59 -0600

    Generate a string containing random hanzi

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -3,14 +3,21 @@ use ferris_says::say;
 use std::io::{stdout, BufWriter};
 
 fn main() {
-    let mut rng = rand::thread_rng();
-    let c = rng.gen_range('\u{4e00}'..'\u{a000}');
-    println!("{}", c);
+  let mut rng = rand::thread_rng();
+  let c = rng.gen_range('\u{4e00}'..'\u{a000}');
+  println!("{}", c);
 
-    let stdout = stdout();
-    let message = String::from("你喜欢吃螃蟹吗？");
-    let width = message.chars().count();
+  let mut v: Vec<String> = Vec::with_capacity(8);
+  for _i in 0..8 {
+    v.push(rng.gen_range('\u{4e00}'..'\u{a000}').to_string());
+  }
+  let message = v.join(", ");
+  println!("{}", message);
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(), width, &mut writer).unwrap();
+  let stdout = stdout();
+  let message = String::from("你喜欢吃螃蟹吗？");
+  let width = message.chars().count();
+
+  let mut writer = BufWriter::new(stdout.lock());
+  say(message.as_bytes(), width, &mut writer).unwrap();
 }
